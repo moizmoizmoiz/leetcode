c24dbdc44fe3e180f9f110bc744ac3e6a6ac1d8b
Moiz <m012n4g4r14@gmail.com>
2022-10-25 17:59:25 +0530

    solved 704 python

---
+++ python3/_704.py
@@ -0,0 +1,21 @@
+"""
+
+
+"""
+
+
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
