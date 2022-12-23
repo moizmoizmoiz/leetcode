da8bc9fa6b23de81d7f4bd4a592d141850250c38
moiz <m012n4g4r14@gmail.com>
2022-12-23 16:24:46 +0530

    _977 for algorithm I badge

---
+++ python3/_977.py
@@ -0,0 +1,24 @@
+# using deque : https://docs.python.org/2.5/lib/deque-objects.html
+# O(n-log-n)
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+
+        # init list as deque O(n) space
+        number_deque = collections.deque(nums)
+        reverse_sorted_squares = []
+
+        while number_deque:
+            # squaring both ends of the deque before appending and popping
+            left_square = number_deque[0] ** 2
+            right_square = number_deque[-1] ** 2
+
+            if left_square > right_square:
+                reverse_sorted_squares.append(left_square)
+                number_deque.popleft()
+            else:
+                reverse_sorted_squares.append(right_square)
+                number_deque.pop()
+
+            # return sorted ascending
+        return reverse_sorted_squares[::-1]
