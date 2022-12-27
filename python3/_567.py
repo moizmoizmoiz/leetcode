f974fc547712f282d0de9b22cfab4308ae45b2fb
moiz <m012n4g4r14@gmail.com>
2022-12-27 22:59:16 +0530

    added 567 for algorithm I badge

---
+++ python3/_567.py
@@ -0,0 +1,23 @@
+# O(n)
+# used hashmaps. the official solution explains all methods including arrays
+# TODO: look into other approaches which do not require hashmaps
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        cntr, w, match = Counter(s1), len(s1), 0
+
+        for i in range(len(s2)):
+            if s2[i] in cntr:
+                if not cntr[s2[i]]: match -= 1
+                cntr[s2[i]] -= 1
+                if not cntr[s2[i]]: match += 1
+
+            if i >= w and s2[i-w] in cntr:
+                if not cntr[s2[i-w]]: match -= 1
+                cntr[s2[i-w]] += 1
+                if not cntr[s2[i-w]]: match += 1
+
+            if match == len(cntr):
+                return True
+
+        return False
